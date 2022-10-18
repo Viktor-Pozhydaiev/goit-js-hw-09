9b2e2e8656c5ccb7ca312334cc8e3a2e704ed9f1
Viktor <jude.peper@gmail.com>
2022-10-18 22:24:48 +0200

    add works files color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const startBtn = document.querySelector('button[data-start="start"]');
+const stopBtn = document.querySelector('button[data-stop="stop"]');
+const body = document.querySelector('body');
 
+let timerId = null;
+
+const startChanging = startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    startBtn.disabled = 'true';
+    body.style.backgroundColor = ` #${Math.floor(
+      Math.random() * 16777215
+    ).toString(16)}`;
+  }, 1000);
+});
+
+const stopChanging = stopBtn.addEventListener('click', () => {
+  startBtn.removeAttribute('disabled');
+  clearInterval(timerId);
+});
