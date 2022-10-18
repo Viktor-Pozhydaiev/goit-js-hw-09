2e57fc30ddc9543c5985f9d9b41f6b5a5474af77
Viktor <jude.peper@gmail.com>
2022-10-18 22:38:33 +0200

    add files in timer/js

---
+++ src/js/02-timer.js
@@ -0,0 +1,12 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
